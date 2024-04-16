f7fca1418888df3f2a9cd7e2a6addc144eb6aab1
remysilva1 <A01663499@tec.mx>
2024-04-16 08:54:40 -0600

    project

--- Snakes_and_ladder/__main__.py
+++ Snakes_and_ladder/__main__.py
@@ -1,10 +1,12 @@
-from board import board
+from board import create_new_board, print_board
 from actions import dice
 
 with open("README.md","r") as f:
    print(f.read())
-    
+
+board = create_new_board()
 while True: 
+    print_board(board)
     command = input("Enter a command:")
     if command == "hello":
         hello() # type: ignore
@@ -12,9 +14,6 @@ while True:
         exit()
     elif command == "throw dice":
         dice()
-    
-    elif command == "b":
-        board(1)
     else:
         print("I did not understand this command.")
     
--- Snakes_and_ladder/board.py
+++ Snakes_and_ladder/board.py
@@ -1,34 +1,28 @@
-def board(positions):
-    turn = 2
-    for x in range (0,100):
-        positions.insert(x, x+1)
-    for y in range(99, -1, -10):
-        if(turn%2==0):
-            print(positions[y],"|",positions[y-1],"|",positions[y-2],"|",positions[y-3],"|",positions[y-4],"|",positions[y-5],"|",positions[y-6],"|",positions[y-7],"|",positions[y-8],"|",positions[y-9])
-            print("-----------------------------------------------------------------------")
-            turn-=1
-        else: 
-            print(positions[y-9],"|",positions[y-8],"|",positions[y-7],"|",positions[y-5],"|",positions[y-4],"|",positions[y-3],"|",positions[y-2],"|",positions[y-1],"|",positions[y])
-            print("-----------------------------------------------------------------------")
-            turn+=1
-def ladderandsnake():
-    print("LADDERS\n 3-24\n 14-42\n 30-86\n 37-57\n 50-96\n 66-74\n")
-    print("SNAKES\n 95-18\n 77-45\n 60-28\n 34-10\n 20-2\n")
-    
-    
 def create_new_board():
-    board = [[" ", " ", " "],
-             [" ", " ", " "],
-             [" ", " ", " "]]
+    board = [[" "]*10]*10
     return board
 
 def print_board(board):
     print(
         f"""
- {board[0][0]} | {board[0][1]} | {board[0][2]}
------------
- {board[1][0]} | {board[1][1]} | {board[1][2]}
------------
- {board[2][0]} | {board[2][1]} | {board[2][2]}
+ {board[0][0]} | {board[0][1]} | {board[0][2]} | {board[0][3]} | {board[0][4]} | {board[0][5]} | {board[0][6]} | {board[0][7]} | {board[0][8]} | {board[0][9]}
+--------------------------------------
+ {board[1][0]} | {board[1][1]} | {board[1][2]} | {board[1][3]} | {board[1][4]} | {board[1][5]} | {board[1][6]} | {board[1][7]} | {board[1][8]} | {board[1][9]}
+--------------------------------------
+ {board[2][0]} | {board[2][1]} | {board[2][2]} | {board[2][3]} | {board[2][4]} | {board[2][5]} | {board[2][6]} | {board[2][7]} | {board[2][8]} | {board[2][9]}
+--------------------------------------
+ {board[3][0]} | {board[3][1]} | {board[3][2]} | {board[3][3]} | {board[3][4]} | {board[3][5]} | {board[3][6]} | {board[3][7]} | {board[3][8]} | {board[3][9]}
+--------------------------------------
+ {board[4][0]} | {board[4][1]} | {board[4][2]} | {board[4][3]} | {board[4][4]} | {board[4][5]} | {board[4][6]} | {board[4][7]} | {board[4][8]} | {board[4][9]}
+--------------------------------------
+ {board[5][0]} | {board[5][1]} | {board[5][2]} | {board[5][3]} | {board[5][4]} | {board[5][5]} | {board[5][6]} | {board[5][7]} | {board[5][8]} | {board[5][9]}
+--------------------------------------
+ {board[6][0]} | {board[6][1]} | {board[6][2]} | {board[6][3]} | {board[6][4]} | {board[6][5]} | {board[6][6]} | {board[6][7]} | {board[6][8]} | {board[6][9]}
+--------------------------------------
+ {board[7][0]} | {board[7][1]} | {board[7][2]} | {board[7][3]} | {board[7][4]} | {board[7][5]} | {board[7][6]} | {board[7][7]} | {board[7][8]} | {board[7][9]}
+--------------------------------------
+ {board[8][0]} | {board[8][1]} | {board[8][2]} | {board[8][3]} | {board[8][4]} | {board[8][5]} | {board[8][6]} | {board[8][7]} | {board[8][8]} | {board[8][9]}
+--------------------------------------
+ {board[9][0]} | {board[9][1]} | {board[9][2]} | {board[9][3]} | {board[9][4]} | {board[9][5]} | {board[9][6]} | {board[9][7]} | {board[9][8]} | {board[9][9]}
 """
     )
